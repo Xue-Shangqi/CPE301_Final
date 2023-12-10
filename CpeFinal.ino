d5f7dea1402bec8449973cd465789e975df4dd4d
Xue-Shangqi <112657781+Xue-Shangqi@users.noreply.github.com>
2023-12-09 22:22:02 -0800

    updated ISR, added different state cases, added display time stamp

--- CpeFinal.ino
+++ CpeFinal.ino
@@ -3,19 +3,26 @@
 //12/2/2023
 
 #include <LiquidCrystal.h>
+#include <DHT.h>
+#include <RTClib.h>
 
-#define RDA
-#define TBE
+#define RDA 0x80
+#define TBE 0x20
+RTC_DS1307 rtc;
 
 // UART Pointers
 volatile unsigned char *myUCSR0A  = (unsigned char *)0x00C0;
 volatile unsigned char *myUCSR0B  = (unsigned char *)0x00C1;
 volatile unsigned char *myUCSR0C  = (unsigned char *)0x00C2;
-volatile unsigned int  *myUBRR0   = (unsigned int *) 0x00C4;
+volatile unsigned int  *myUBRR0   = (unsigned int  *)0x00C4;
 volatile unsigned char *myUDR0    = (unsigned char *)0x00C6;
 // GPIO Pointers
 volatile unsigned char *portB     = (unsigned char*) 0x25;
-volatile unsigned char *portDDRB  = (unsigned char*) 0x24;
+volatile unsigned char *ddr_b     = (unsigned char*) 0x24;
+volatile unsigned char *portE     = (unsigned char*) 0x2E;
+volatile unsigned char *ddr_e     = (unsigned char*) 0x2D;
+volatile unsigned char *pin_e     = (unsigned char*) 0x2C;
+
 // Timer Pointers
 volatile unsigned char *myTCCR1A  = (unsigned char *) 0x80;
 volatile unsigned char *myTCCR1B  = (unsigned char *) 0x81;
@@ -29,91 +36,128 @@ volatile unsigned char* my_ADCSRB = (unsigned char*) 0x7B;
 volatile unsigned char* my_ADCSRA = (unsigned char*) 0x7A;
 volatile unsigned int* my_ADC_DATA = (unsigned int*) 0x78;
 
+//global variables
+double temperature = -1;
+double humidity = -1;
+double waterlevel = -1;
+int prevState = 0;
+
+//state of the refrigerator
+enum State{
+  disabled = 0,
+  idle = 1,
+  running = 2,
+  error = 3
+};
+
+State state = disabled;
+
+//setup temperature sensor
+DHT dht(2, DHT11);
+
 
 //global ticks counter
-
 void setup() 
-{                
-  // set PB6 to output
-  *portDDRB |= 0x40;
-  // set PB6 LOW
-  *portB &= 0xBF;
+{ 
+  //set PE5 (start button) to input             
+  *ddr_e &= 0xDF;
+  *portE |= 0x20;
+
+
+  // set PB5,6,7 (RGB) to output
+  *ddr_b |= 0x20;
+  *ddr_b |= 0x40;
+  *ddr_b |= 0x80;
+
+
   // setup the Timer for Normal Mode, with the TOV interrupt enabled
-  setup_timer_regs();
+  //setup_timer_regs();
   // Start the UART
   U0Init(9600);
   //setup the ADC
   adc_init();
+  //Serial.begin(9600);
+  rtc.begin();
+  rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  attachInterrupt(digitalPinToInterrupt(3), start, RISING);
 }
 
 void loop() 
 {
+  if(state != prevState){
+    displayTimeStamp();
+    prevState = state;
+  }
+
+  switch(state){
+    case 1:
+      temperature = dht.readTemperature();
+      
+      //change LED to green
+      *portB &= !0x01 << 5;
+      *portB |= 0x01 << 6;
+      *portB &= !0x01 << 7;
+      break;
+    case 2:
+
+      break;
+    case 3:
+
+      //change LED to red
+      *portB &= !0x01 << 5;
+      *portB &= !0x01 << 6;
+      *portB |= 0x01 << 7;
+      break;
+    case 0:
+
+      //change LED to yellow 
+      *portB &= !0x01 << 5;
+      *portB |= 0x01 << 6;
+      *portB |= 0x01 << 7;
+      break;
+  }
 
 }
 
-
-// Timer setup function
-void setup_timer_regs()
-{
-  // setup the timer control registers
-  *myTCCR1A= 0x00;
-  *myTCCR1B= 0X00;
-  *myTCCR1C= 0x00;
-  
-  // reset the TOV flag
-  *myTIFR1 |= 0b00000001;
-  
-  // enable the TOV interrupt
-  *myTIMSK1 |= 0b00100000;
-}
-
-
-// TIMER OVERFLOW ISR
-ISR(TIMER1_OVF_vect)
-{
-  // Stop the Timer
-  *myTCCR1B &= 0xF8;
-  // Load the Count
-  *myTCNT1 =  (unsigned int) (65535 -  (unsigned long) (currentTicks));
-  // Start the Timer
-  *myTCCR1B |= 0b00000001;
-  // if it's not the STOP amount
-  if(currentTicks != 65535)
-  {
-    // XOR to toggle PB6
-    *portB ^= 0x40;
+void start(){
+  if(state == disabled){
+    state = idle;
+  }else{
+    state = disabled;
   }
 }
+
 /*
  * UART FUNCTIONS
  */
-void U0Init(int U0baud)
+void U0Init(unsigned int U0baud)
 {
  unsigned long FCPU = 16000000;
  unsigned int tbaud;
  tbaud = (FCPU / 16 / U0baud - 1);
- // Same as (FCPU / (16 * U0baud)) - 1;
  *myUCSR0A = 0x20;
  *myUCSR0B = 0x18;
  *myUCSR0C = 0x06;
  *myUBRR0  = tbaud;
 }
 
-unsigned char kbhit()
+unsigned char U0kbhit()
 {
   return *myUCSR0A & RDA;
 }
 
-unsigned char getChar()
+unsigned char U0getChar()
 {
+  while (!(*myUCSR0A & RDA)) ;
   return *myUDR0;
 }
 
-void putChar(unsigned char U0pdata)
+void U0putChar(unsigned char U0pdata)
 {
-  while((*myUCSR0A & TBE)==0);
+  while (!(*myUCSR0A & TBE));
   *myUDR0 = U0pdata;
 }
+
 
 /* Analog Port functions*/
 void adc_init()
@@ -155,4 +199,13 @@ unsigned int adc_read(unsigned char adc_channel_num)
   while((*my_ADCSRA & 0x40) != 0);
   // return the result in the ADC data register
   return *my_ADC_DATA;
+}
+
+void displayTimeStamp(){
+  DateTime now = rtc.now();
+  String date;
+  date = now.toString("YYYY-MM-DD hh:mm:ss");
+  for(int i=0; i<date.length(); ++i){
+    U0putChar(date[i]);
+  }
 }
